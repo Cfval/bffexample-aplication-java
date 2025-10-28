b4e7fbe49afd9b1a60ab87d3aec5e026d782e35d
USERSAD\cfrancev <cfrancev@emeal.nttdata.com>
2025-10-28 11:46:22 +0100

    Primer commit - proyecto Spring Boot base

---
+++ src/main/java/com/cfv/bff/BffexampleAplicationJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cfv.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffexampleAplicationJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffexampleAplicationJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfv/bff/BffexampleAplicationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfv.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffexampleAplicationJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
